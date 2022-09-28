0da6c9e14bf253d0c08d6d60dcd8166a0d3a3ca6
David Fortich <d4tich@gmail.com>
2022-09-28 08:39:28 -0400

    Added Pet and PetType to Model:

--- PetClinic/src/main/java/ca/gbc/comp3095/petclinic/model/Owner.java
+++ PetClinic/src/main/java/ca/gbc/comp3095/petclinic/model/Owner.java
@@ -1,7 +1,4 @@
 package ca.gbc.comp3095.petclinic.model;
 
 public class Owner extends Person{
-    public Owner(String firstName, String lastName) {
-        super(firstName, lastName);
-    }
 }
--- PetClinic/src/main/java/ca/gbc/comp3095/petclinic/model/Vet.java
+++ PetClinic/src/main/java/ca/gbc/comp3095/petclinic/model/Vet.java
@@ -1,7 +1,4 @@
 package ca.gbc.comp3095.petclinic.model;
 
 public class Vet extends Person{
-    public Vet(String firstName, String lastName) {
-        super(firstName, lastName);
-    }
 }
